1550d107eb5d3e5563e8847456f5f78cd1400917
samuel <ribeiro.samuel77@gmail.com>
2023-08-03 02:00:18 -0300

    Implementado conexão ao banco de dados que está subindo no docker via linux server

--- src/main/kotlin/me/tqi/dto/request/ProdutoDto.kt
+++ src/main/kotlin/me/tqi/dto/request/ProdutoDto.kt
@@ -8,14 +8,12 @@ import me.tqi.entity.Produto
 data class ProdutoDto(
     @field:NotEmpty(message = "Invalid input") val nomeProduto: String,
     val precoUnitario: Double,
-    @field:NotEmpty(message = "Invalid input") val nomeCategoria: String,
     @field:NotNull(message = "Invalid input") val idCategoria: Long
 ){
     fun toEntity(): Produto = Produto(
         nomeProduto = this.nomeProduto,
         precoUnitario = this.precoUnitario,
         categoria = Categoria(
-            nomeCategoria = this.nomeCategoria,
             id = this.idCategoria
         )
     )
