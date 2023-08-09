8620547b8e57935735e96d0a07c83e7e1befb36f
samuel <ribeiro.samuel77@gmail.com>
2023-08-09 01:55:04 -0300

    Ajuste em uma das querys de consulta na tabela de carrinho.

--- src/main/kotlin/me/tqi/repository/CarrinhoRepository.kt
+++ src/main/kotlin/me/tqi/repository/CarrinhoRepository.kt
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query
 
 interface CarrinhoRepository : JpaRepository<Carrinho, Long> {
 
-    @Query(value = "SELECT * FROM CARRINHO WHERE USUARIO_ID = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM carrinho WHERE USUARIO_ID = ?1", nativeQuery = true)
     fun findByIdUsuarioCarrinho(idUsuario: Long): List<Carrinho>
 
 
