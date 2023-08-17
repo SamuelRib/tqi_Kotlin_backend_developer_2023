a069516b140d9ffedf66678ab8ced20084ba3d51
samuel <ribeiro.samuel77@gmail.com>
2023-08-17 15:34:20 -0300

    Teste criação carrinho.

--- src/test/kotlin/me/tqi/service/CarrinhoServiceTest.kt
+++ src/test/kotlin/me/tqi/service/CarrinhoServiceTest.kt
@@ -4,6 +4,8 @@ import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.just
+import io.mockk.runs
 import io.mockk.verify
 import me.tqi.entity.Carrinho
 import me.tqi.entity.Produto
@@ -14,6 +16,7 @@ import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.test.context.ActiveProfiles
+import java.util.*
 
 @ActiveProfiles("test")
 @ExtendWith(MockKExtension::class)
@@ -36,6 +39,47 @@ class CarrinhoServiceTest {
         Assertions.assertThat(actual).isSameAs(fakeCarrinho)
         verify(exactly = 1) { carrinhoRepository.save(fakeCarrinho) }
 
+    }
+
+    @Test
+    fun `should find carrinho by id`() {
+        //given
+        val fakeId: Long = Random().nextLong()
+        val fakeCarrinho: Carrinho = builderCarrinho(fakeId)
+        every { carrinhoRepository.findById(fakeId) } returns Optional.of(fakeCarrinho)
+        //when
+        val actual: Carrinho = carrinhoService.findByIdCarrinho(fakeId)
+        //then
+        Assertions.assertThat(actual).isNotNull
+        Assertions.assertThat(actual).isExactlyInstanceOf(Carrinho::class.java)
+        Assertions.assertThat(actual).isSameAs(fakeCarrinho)
+        verify(exactly = 1) { carrinhoRepository.findById(fakeId) }
+    }
+
+    @Test
+    fun `should not find carrinho by invalid id and RuntimeException`() {
+        //given
+        val fakeId: Long = Random().nextLong()
+        every { carrinhoRepository.findById(fakeId) } returns Optional.empty()
+        //then
+        Assertions.assertThatExceptionOfType(RuntimeException::class.java)
+            .isThrownBy { carrinhoService.findByIdCarrinho(fakeId) }
+            .withMessage("idCarrinho $fakeId not found")
+        verify(exactly = 1) { carrinhoRepository.findById(fakeId) } // verifica se está sendo chamado apenas 1 vez
+    }
+
+    @Test
+    fun `should delete carrinho by id`() {
+        //given
+        val fakeId: Long = Random().nextLong()
+        val fakeCarrinho: Carrinho = builderCarrinho(fakeId) //é o id salvo no banco de dados
+        every { carrinhoRepository.findById(fakeId) } returns Optional.of(fakeCarrinho)
+        every { carrinhoRepository.delete(fakeCarrinho) } just runs //como não vai retornar nada, usamos o just runs para indicar
+        //when
+        carrinhoService.deleteCarrinho(fakeId)
+        //then
+        verify(exactly = 1) { carrinhoRepository.findById(fakeId) }
+        verify(exactly = 1) { carrinhoRepository.delete(fakeCarrinho) }
     }
 }
 
